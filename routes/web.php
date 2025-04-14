8d40dd319a4a71d5a76ce50b805521c37da14284
Hafiz-azhar1 <hafiz.azhar@csc.net.my>
2025-04-14 13:17:02 +0800

    ke dashboard

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
         $request->fulfill();
-        return redirect()->route('home')->with('status', 'Email verified successfully!');
+        return redirect()->route('dashboard')->with('status', 'Email verified successfully!');
     })->middleware(['signed'])->name('verification.verify');
 
     Route::post('/email/verification-notification', function (Request $request) {
