36c837a0916b2ae12f7ccffd54df10b9d20e1ab4
Hafiz-azhar1 <hafiz.azhar@csc.net.my>
2025-04-25 17:51:07 +0800

    penukaran untuk no tel majikan

--- resources/views/livewire/module/maklumat-peribadi.blade.php
+++ resources/views/livewire/module/maklumat-peribadi.blade.php
@@ -776,15 +776,19 @@
 
                             <div class="col-span-6 sm:col-span-2">
                                 <label for="employer_phone" class="block text-sm font-medium leading-5 text-gray-700">No Tel Majikan <span class="text-red-700">*</span></label>
-                                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                    <span class="text-gray-500 sm:text-sm sm:leading-5">
-                                        +6
-                                    </span>
+                                <div class="relative mt-1 rounded-md shadow-sm">
+                                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                                        <span class="text-gray-500 sm:text-sm sm:leading-5">
+                                            +6
+                                        </span>
+                                    </div>
+                                    <input id="employer_phone" name="employer_phone" class="block px-3 py-2 pl-16 mt-1 w-full rounded-md border border-gray-300 shadow-sm transition duration-150 ease-in-out form-input sm:pl-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5" minlength="10" maxlength="11"  wire:model.live="employer_phone">
+                                    @error('employer_phone')
+                                    <p class="text-red-500 text-xs italic mt-4">
+                                        {{ $message }}
+                                    </p>
+                                    @enderror
                                 </div>
-                                <input id="employer_phone" name="employer_phone" class="block px-3 py-2 pl-16 mt-1 w-full rounded-md border border-gray-300 shadow-sm transition duration-150 ease-in-out form-input sm:pl-14 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5" minlength="10" maxlength="11"  wire:model.live="employer_phone">
-                                @error('employer_phone')
-                                <p class="text-red-500 text-xs italic mt-4">{{ $message }}</p>
-                                @enderror
                             </div>
                             @endif
                         </div>
